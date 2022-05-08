11d75cbfdd1c98d2c16f56682e2e9e4b5d94f46b
Dokome <1098265675@qq.com>
2022-05-08 19:49:28 +0800

    submenu test complete

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react'
 import Menu, { MenuMode, MenuProps } from './menu'
 import MenuItem from './menuItem'
+import SubMenu from './subMenu'
 
 const testProps: MenuProps = {
   defaultIndex: '0',
@@ -17,21 +24,40 @@ const testVerticalProps: MenuProps = {
 const generateMenu = (props: MenuProps) => {
   return (
     <Menu {...props}>
-      <MenuItem index={'0'}>Active MenuItem</MenuItem>
-      <MenuItem index={'1'} disabled>
-        Disabled MenuItem
-      </MenuItem>
-      <MenuItem index={'2'}>Normal MenuItem</MenuItem>
+      <MenuItem>Active MenuItem</MenuItem>
+      <MenuItem disabled>Disabled MenuItem</MenuItem>
+      <MenuItem>Normal MenuItem</MenuItem>
+      <SubMenu>
+        <MenuItem>SubMenu MenuItem</MenuItem>
+      </SubMenu>
     </Menu>
   )
 }
+
+const createStyleFile = () => {
+  const cssFile: string = `
+    .submenu {
+      display: none;
+    }
+    .submenu.menu-opened {
+      display: block;
+    }
+  `
+  const style = document.createElement('style')
+  style.innerHTML = cssFile
+  return style
+}
+
 let MenuElement: HTMLElement,
   ActiveElement: HTMLElement,
   DisabledElement: HTMLElement
 describe('menu and menuItem component', () => {
   beforeEach(() => {
     // eslint-disable-next-line testing-library/no-render-in-setup
-    render(generateMenu(testProps))
+    const view = render(generateMenu(testProps))
+    // eslint-disable-next-line testing-library/no-container
+    view.container.append(createStyleFile())
+
     MenuElement = screen.getByTestId('test-menu')
     ActiveElement = screen.getByText('Active MenuItem')
     DisabledElement = screen.getByText('Disabled MenuItem')
@@ -39,8 +65,9 @@ describe('menu and menuItem component', () => {
   it('should render the correct menu and menuItem based on default props', () => {
     expect(MenuElement).toBeInTheDocument()
     expect(MenuElement).toHaveClass('menu test')
+    // expect(MenuElement.getElementsByTagName('li').length).toEqual(3)
     // eslint-disable-next-line testing-library/no-node-access
-    expect(MenuElement.getElementsByTagName('li').length).toEqual(3)
+    expect(MenuElement.querySelectorAll(':scope > li').length).toEqual(4)
     expect(ActiveElement).toHaveClass('menu-item is-active')
     expect(DisabledElement).toHaveClass('menu-item is-disabled')
   })
@@ -61,5 +88,20 @@ describe('menu and menuItem component', () => {
     render(generateMenu(testVerticalProps))
     MenuElement = screen.getByTestId('test-menu')
     expect(MenuElement).toHaveClass('menu-vertical')
+  })
+
+  it('should show dropdown items when hover on subMenu', async () => {
+    expect(screen.queryByText('SubMenu MenuItem')).not.toBeVisible()
+    const dropdownElement = screen.getByText('SubMenu MenuItem')
+    fireEvent.mouseEnter(dropdownElement)
+    await waitFor(() => {
+      expect(screen.queryByText('SubMenu MenuItem')).toBeVisible()
+    })
+    fireEvent.click(screen.getByText('SubMenu MenuItem'))
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+    fireEvent.mouseLeave(dropdownElement)
+    await waitFor(() => {
+      expect(screen.queryByText('SubMenu MenuItem')).not.toBeVisible()
+    })
   })
 })
